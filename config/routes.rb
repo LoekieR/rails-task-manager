b4ad220f4ab35e7c3a8a472feeda07906d66bc15
Annelieke Roeleveld <anneliekeroeleveld+github@gmail.com>
2023-01-18 17:26:50 +0100

    will try again on weekend

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root 'tasks#index'
+  # Read all
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # Read one - The `show` route needs to be *after* `new` route.
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
